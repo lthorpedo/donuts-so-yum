43d02a72f9568373eaba0134cacce04f5b16cb13
lthorpedo <lthorpedo@users.noreply.github.com>
2020-12-09 12:27:42 -0700

    bootstrap styles work

--- src/App.js
+++ src/App.js
@@ -3,14 +3,13 @@ import './App.scss'
 import Main from "./Main"
 import {
   BrowserRouter as Router,
-  Route, Switch
+  Route
 } from "react-router-dom"
 
 export default function App() {
   return (
     <Router>
-      <Route>
-        <Main />
+      <Route path='/' component={(props) => <Main {...props} key={window.location.pathname}/>}>
       </Route>
     </Router>
   );
--- src/DonutTile.js
+++ src/DonutTile.js
@@ -4,7 +4,7 @@ import React from "react";
 export const DonutTile = ({donut}) => {
   return (
     <>
-    <div className="donut_tile col-md-4">
+    <div className="donut_tile col-md-2">
         <img width="200px" height="200px" src={donut.imageurl} alt="Not Found"></img>
         <p>{donut.name}</p>
     </div>
